8c7ac598ef54ed4b1cef47c4ea3d6085f0fc1902
Robin Duckett <robin.duckett@mac.com>
2010-01-23 16:59:02 +0000

    Removed old external dir

--- external/dwoo/compiled/C-/var/www/vhosts/locus/views/layout/default.tpl.d16.php
+++
@@ -1,8 +0,0 @@
-<?php
-ob_start(); /* template body */ ?>This is the default layout<br />
-The title is <?php echo $this->scope["title"];?><br />
-<?php echo $this->scope["content"];?> goes here<br />
-And here is stuff under the content.
-<?php  /* end template body */
-return $this->buffer . ob_get_clean();
-?>
--- external/dwoo/compiled/C-/var/www/vhosts/locus/views/pages/index.tpl.d16.php
+++
@@ -1,5 +0,0 @@
-<?php
-ob_start(); /* template body */ ?>This is index content and it
-<?php  /* end template body */
-return $this->buffer . ob_get_clean();
-?>
